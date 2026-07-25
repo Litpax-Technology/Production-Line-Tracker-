be610e5591aca13af73845e61ad96021dafb3afd
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-25 17:47:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 var CONFIG = {
 
   // Apps Script Web App /exec URL (Deploy > New deployment > Web app)
-  API_URL: 'https://script.google.com/macros/s/AKfycbx2se5RRR_HBoMF2SDUULVb8IIdPNTH02BnUrFdQU99i8NA8Ps5g4tOQOFjee5vznxUSQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwHOyyUxn7mt7MQljnkxgCS0hDqDlPaNpLY-aIFfI_dHPTcCV4hcooKBsc23qKYpc6uyw/exec',
 
   // Which floor/unit this PC belongs to. Goes into every log row so all
   // floors can share one Sheet but each PC only sees its own work.
